d8af6dcf166b511c42027a3a18899e66cb3004f6
casey cole <caseycole589@gmail.com>
2025-08-01 16:20:54 -0500

    functions copied over from first project

--- main.cpp
+++ main.cpp
@@ -1,5 +1,65 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::abs;
 using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class State { kEmpty, kObstacle };
+
+vector<State> ParseLine(string line) {
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',') {
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
+    }
+  }
+
+  return row;
+}
+
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile(path);
+  vector<vector<State>> board;
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+string CellString(State cell) {
+  switch (cell) {
+  case State::kObstacle:
+    return "⛰️   ";
+  default:
+    return "0   ";
+  }
+}
+
+// for visualizing
+void PrintBoard(const vector<vector<State>> &board) {
+  for (auto row : board) {
+    for (auto j : row) {
+      cout << CellString(j);
+    }
+    cout << "\n";
+  }
+}
 
 int main() {
   cout << "hello world";
